9d7baea3cca7d6c3562e7b0f9043a0585e97acea
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-16 10:35:54 +0900

    Implement the text section

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -63,6 +76,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
