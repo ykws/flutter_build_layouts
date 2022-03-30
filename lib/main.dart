d3454c3569d24f1fa13bfd843dfa198bc1dd4c58
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2022-03-30 17:35:41 +0900

    Implement the button row

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,17 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -52,10 +63,32 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            titleSection
+            titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
